a8c377fcd9366cbf1bcb0d5ee2a41be3d47c001b
rai22474 <rai22474@yahoo.es>
2014-04-03 08:35:06 +0200

    Solve race conditions in getCollection method.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 #hydra-java-mongo
 
-The mongo wrapper for hydra
-
-This project use hydra to make the connection with mongo resilent.
+The main propose of this class is manage a mongodb connection using hydra as a source of the mongo server ip.
+Every request check if the list of servers has changes, if this happens create a new connection to mongo
+database with the new server address list. The previous connection remains open until the next hydra server change
+in order to fulfills the running connection requests.  
--- src/main/java/io/github/innotech/hydra/mongo/HydratedMongoDB.java
+++ src/main/java/io/github/innotech/hydra/mongo/HydratedMongoDB.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.ws.rs.core.UriBuilder;
 
@@ -18,14 +19,23 @@ import com.mongodb.ReadPreference;
 import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcern;
 
+/**
+ * The main propose of this class is manage a mongodb connection using hydra as
+ * a source of the mongo server ip. Every request check if the list of servers
+ * has changes, if this happens create a new connection to mongo database with
+ * the new server address list. The previous connection remains open until the
+ * next hydra server change in order to fulfills the running connection
+ * requests.
+ * 
+ */
 public class HydratedMongoDB {
-	
+
 	private DB mongoDatabase;
-	
+
 	private String applicationName;
-	
+
 	private String databaseName;
-	
+
 	private HydraClient hydraClient;
 
 	private LinkedHashSet<String> servers = new LinkedHashSet<String>();
@@ -33,30 +43,57 @@ public class HydratedMongoDB {
 	private MongoClient mongoClient;
 
 	private ReadPreference readPreference = ReadPreference.primary();
-	
+
 	private WriteConcern writeConcern = WriteConcern.NORMAL;
-	
-	//The connection with mongo remains open until the next hydra server change for not destroy the running request 
-	//with mongo in this connection.
+
+	private ReentrantLock mutex = new ReentrantLock();
+
+	// The connection with mongo remains open until the next hydra server change
+	// for not destroy the running request
+	// with mongo in this connection.
 	private MongoClient staleMongoClient;
-	
+
 	public HydratedMongoDB(String applicationName, String databaseName, HydraClient hydraClient) {
 		this.hydraClient = hydraClient;
 		this.applicationName = applicationName;
 		this.databaseName = databaseName;
 	}
 
+	/**
+	 * Method that initialize the mongo connection. The call of this method not
+	 * is mandatory. If not call the first call to getColection initialize.
+	 */
+	public void initMongoConnection() {
+		if (mutex.tryLock()) {
+			try {
+				servers = hydraClient.get(applicationName);
+				createNewMongoClient();
+			} finally {
+				mutex.unlock();
+			}
+		}
+	}
+
+	/**
+	 * Return the mongo collection with the given name. Initialize the
+	 * connection if not exist.
+	 */
 	public DBCollection getCollection(String collectionName) {
 		LinkedHashSet<String> newServers = hydraClient.get(applicationName);
-		
-		if (newServers.isEmpty()){
+
+		if (newServers.isEmpty()) {
 			throw new IllegalStateException("Hydra not found any active mongo servers.");
 		}
-		
-		if (!servers.equals(newServers)){
-			swapMongoServers(newServers);
+
+		try {
+			mutex.lock();
+			if (!servers.equals(newServers)) {
+				swapMongoServers(newServers);
+			}
+		} finally {
+			mutex.unlock();
 		}
-	
+
 		return mongoDatabase.getCollection(collectionName);
 	}
 
@@ -65,30 +102,34 @@ public class HydratedMongoDB {
 
 		servers = newServers;
 		staleMongoClient = mongoClient;
-		
+
+		createNewMongoClient();
+
+		if (oldMongoClient != null) {
+			oldMongoClient.close();
+		}
+
+		mongoDatabase = mongoClient.getDB(databaseName);
+	}
+
+	private void createNewMongoClient() {
 		mongoClient = new MongoClient(createServerAddress(servers));
 		mongoClient.setReadPreference(readPreference);
 		mongoClient.setWriteConcern(writeConcern);
-		
-		if (oldMongoClient != null){
-			oldMongoClient.close();
-		}
-		
-		mongoDatabase = mongoClient.getDB(databaseName);
 	}
-	
+
 	private List<ServerAddress> createServerAddress(Collection<String> servers) {
 		List<ServerAddress> serverAdresses = new ArrayList<ServerAddress>();
-		
+
 		for (String server : servers) {
 			try {
 				URI uri = UriBuilder.fromPath(server).build();
-				serverAdresses.add(new ServerAddress(uri.getHost(),uri.getPort()));
+				serverAdresses.add(new ServerAddress(uri.getHost(), uri.getPort()));
 			} catch (UnknownHostException e) {
 				throw new IllegalStateException();
 			}
 		}
-		
+
 		return serverAdresses;
 	}
 
@@ -106,5 +147,5 @@ public class HydratedMongoDB {
 
 	public void setWriteConcern(WriteConcern writeConcern) {
 		this.writeConcern = writeConcern;
-	}	
+	}
 }
